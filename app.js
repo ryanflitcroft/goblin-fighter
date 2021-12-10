6f6722b332388c7a22b70403b171ff21fa127ed5
Ryan Flitcroft <ryanflitcroft@ymail.com>
2021-12-10 11:57:26 -0800

    renderGoblin and displayGoblin functions written

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { renderGoblin, displayGoblins, renderDefeatedGoblins, renderFighter} from '';
+import { renderGoblin, renderDefeatedGoblins, renderFighter} from './render-utils.js';
 
 const goblinForm = document.querySelector('#goblin-form');
 const goblinDataSection = document.querySelector('#goblin-data-section');
@@ -9,29 +9,73 @@ const fighterContainer = document.querySelector('#fighter-container');
 
 const goblinArray = [
     {
-        name: 'xoo',
-        HP: 3
+        goblinName: 'xoo',
+        goblinHP: 3
     },
     {
-        name: 'xyy',
-        HP: 3
+        goblinName: 'xyy',
+        goblinHP: 3
     }
 ];
 
+let goblinName;
+let goblinHP = 3;
+
 let defeatedGoblinCount = 0;
 let fighterHP = 13;
-
 // console.log(goblinArray, defeatedGoblinCount, fighterHP);
 
 goblinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    const data = new FormData(goblinForm);
+    goblinName = data.get('name');
+    // console.log(goblinName);
+
+    goblinForm.reset();
+
+    const goblinObject = {
+        goblinName,
+        goblinHP
+    };
+
+    goblinArray.push(goblinObject);
+    displayGoblins();
 });
 
-for (let goblinObject of goblinArray) {
 
-    goblinObject.addEventListener('click', () => {
 
-    });
+function displayGoblins() {
+    // console.log(goblinArray);
+    goblinDataSection.textContent = '';
+
+    for (let goblin of goblinArray) {
+
+        const goblinEl = renderGoblin(goblin);
+    
+        goblinEl.addEventListener('click', () => {
+            let strike = Math.floor(Math.random() * 10);
+            // console.log(strike);
+            if (strike > 4) {
+                alert(`You have struck ${goblin.goblinName}!!`);
+                goblin.goblinHP--;
+                // console.log(goblin);
+            } else if (strike < 4) {
+                alert(`You tried to strike ${goblin.goblinName} and missed, ${goblin.goblinName} has attacked you!!`);
+                fighterHP--;
+                // console.log(goblin);
+            } else {
+                alert(`You tried to strike ${goblin.goblinName} and missed, ${goblin.goblinName} has attacked you... but missed!!`);
+                // console.log(goblin);
+            }
+
+            displayGoblins();
+        });
+    
+        goblinDataSection.append(goblinEl);
+    
+    }
 
 }
 
+displayGoblins();
--- render-utils.js
+++ render-utils.js
@@ -1,13 +1,31 @@
-export function renderGoblin() {
+export function renderGoblin(goblinData) {
+    const goblinContainer = document.createElement('div');
+    const goblinEmoji = document.createElement('span');
+    const goblinName = document.createElement('span');
+    const goblinHP = document.createElement('span');
 
+    goblinContainer.classList.add('goblin-container');
+    goblinEmoji.classList.add('goblin', 'emoji');
+    goblinName.classList.add('goblin');
+    goblinHP.classList.add('goblin');
+
+    goblinEmoji.textContent = '👹';
+    goblinName.textContent = goblinData.goblinName;
+    goblinHP.textContent = `${goblinData.goblinHP} hp`;
+
+    goblinContainer.append(goblinEmoji, goblinName, goblinHP);
+    return goblinContainer;
 }
 
-export function displayGoblins() {
+export function renderDefeatedGoblins(defeatedData) {
+    // const defeatedContainer = document.createElement('div');
+    // const defeatedCount = document.createElement('span');
+    // const defeatedEmoji = document.createElement('span');
 
-}
+    // defeatedContainer.classList.add('defeated-goblin-container');
 
-export function renderDefeatedGoblins() {
-
+    // defeatedContainer.textContent = #;
+    // defeatedEmoji.textContent = '💀';
 }
 
 export function renderFighter() {
